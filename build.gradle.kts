7ab992e8908b8aeed92305fc80d8ec5b130f4444
Umut Savas <daniel230994@gmail.com>
2022-12-18 16:36:53 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.0"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.5.0"
+	kotlin("jvm") version "1.7.21"
+	kotlin("plugin.spring") version "1.7.21"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+	maven(url = "https://packages.confluent.io/maven/")
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.apache.kafka:kafka-streams")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.avro:avro:1.11.1")
+	implementation("org.apache.kafka:kafka-clients:3.3.1")
+	implementation("io.confluent:kafka-avro-serializer:7.3.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+sourceSets["main"].java.srcDir(file("build/generated-main-avro-java"))
---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,49 @@
+package com.example.demo
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.kafka.clients.producer.Callback
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.lang.Exception
+import java.util.*
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+
+	val props = Properties()
+	props.setProperty("bootstrap.servers", "127.0.0.1:29092")
+	props.setProperty("acks", "1")
+	props.setProperty("retries", "10")
+	props.setProperty("key.serializer", StringSerializer::class.java.name)
+	props.setProperty("value.serializer", KafkaAvroSerializer::class.java.name)
+	props.setProperty("schema.registry.url", "http://127.0.0.1:8085")
+
+	val kafkaProducer = KafkaProducer<String, Employee>(props)
+	val topic = "employee-avro"
+
+	val employee = Employee.newBuilder()
+		.setName("Umut")
+		.setAge(28)
+		.build()
+
+	val producerRecord = ProducerRecord<String, Employee>(topic, employee)
+	kafkaProducer.send(producerRecord) { metadata, exception ->
+
+		if (exception == null) {
+			println("Success!")
+			println(metadata.toString())
+		} else {
+			exception.printStackTrace()
+		}
+	}
+
+	kafkaProducer.flush()
+	kafkaProducer.close()
+}
